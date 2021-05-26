af0e4ac1cd112bf08d881c8a208720e2c4a2848b
stephane888 <kksasteph888@gmail.com>
2021-05-26 16:21:43 +0100

    MAJ

--- src/EventSubscriber/AjaxStatusSubscriber.php
+++ src/EventSubscriber/AjaxStatusSubscriber.php
@@ -13,51 +13,55 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 class AjaxStatusSubscriber implements EventSubscriberInterface {
 
-  /**
-   * The messenger.
-   *
-   * @var \Drupal\Core\Messenger\MessengerInterface
-   */
-  protected $messenger;
+	/**
+	 * The messenger.
+	 *
+	 * @var \Drupal\Core\Messenger\MessengerInterface
+	 */
+	protected $messenger;
 
-  /**
-   * Constructs event subscriber.
-   *
-   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   *   The messenger.
-   */
-  public function __construct(MessengerInterface $messenger) {
-    $this->messenger = $messenger;
-  }
+	/**
+	 * Constructs event subscriber.
+	 *
+	 * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+	 *        	The messenger.
+	 */
+	public function __construct(MessengerInterface $messenger) {
+		$this->messenger = $messenger;
+	}
 
-  /**
-   * Kernel request event handler.
-   *
-   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
-   *   Response event.
-   */
-  public function onKernelRequest(GetResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
-  }
+	/**
+	 * Kernel request event handler.
+	 *
+	 * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+	 *        	Response event.
+	 */
+	public function onKernelRequest(GetResponseEvent $event) {
+		// $this->messenger->addStatus(__FUNCTION__);
+	}
 
-  /**
-   * Kernel response event handler.
-   *
-   * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
-   *   Response event.
-   */
-  public function onKernelResponse(FilterResponseEvent $event) {
-    $this->messenger->addStatus(__FUNCTION__);
-  }
+	/**
+	 * Kernel response event handler.
+	 *
+	 * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
+	 *        	Response event.
+	 */
+	public function onKernelResponse(FilterResponseEvent $event) {
+		// $this->messenger->addStatus(__FUNCTION__);
+	}
 
-  /**
-   * {@inheritdoc}
-   */
-  public static function getSubscribedEvents() {
-    return [
-      KernelEvents::REQUEST => ['onKernelRequest'],
-      KernelEvents::RESPONSE => ['onKernelResponse'],
-    ];
-  }
-
+	/**
+	 *
+	 * {@inheritdoc}
+	 */
+	public static function getSubscribedEvents() {
+		return [ 
+				KernelEvents::REQUEST => [ 
+						'onKernelRequest'
+				],
+				KernelEvents::RESPONSE => [ 
+						'onKernelResponse'
+				]
+		];
+	}
 }
