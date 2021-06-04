fb62dcbb86eaa507285913bc06e1be9a3e051d5c
stephane888 <kksasteph888@gmail.com>
2021-06-04 04:10:14 +0100

    MAJ

--- src/Services/Codes.php
+++ src/Services/Codes.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace Drupal\ajax_status\Services;
 
 class Codes {
-
-  private $code = 200;
-
-  function setCode($value = 200)
-  {
-    $this->code = $value;
-  }
-
-  function getCode()
-  {
-    return $this->code;
-  }
+	private $code = 200;
+	function setCode($value = 200){
+		$this->code = $value;
+	}
+	function getCode(){
+		return $this->code;
+	}
+	function hasErrors(){
+		if($this->code !== 200)
+			return true;
+		else
+			false;
+	}
 }
